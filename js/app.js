bdc5c3371c86967914aaa0bd44680094ea557962
Joseph McClenahan <jmcclena94@gmail.com>
2016-01-13 13:36:42 -0800

    Added image, updated table

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@ function shopLocData(location,minCustomers,maxCustomers,cupsPer,poundsPer) {
   this.cupLbs = [];
   this.rawLbs = [];
   this.totLbs = [];
+  var tempCupLbsTotal = 0;
+  var tempRawLbsTotal = 0;
+  var tempDayLbsTotal = 0;
   this.calculator = function () {
     for (var i = 0; i < hoursString.length; i++) {
       var lbsPerCup = 0.05;
@@ -26,12 +29,18 @@ function shopLocData(location,minCustomers,maxCustomers,cupsPer,poundsPer) {
       var tempCupLbs = tempCups * lbsPerCup;
       var tempRawLbs = randomCustomers * poundsPer;
       var tempTotLbs = tempCupLbs + tempRawLbs;
+      tempCupLbsTotal = tempCupLbsTotal + tempCupLbs;
+      tempRawLbsTotal = tempRawLbsTotal + tempRawLbs;
+      tempDayLbsTotal = tempDayLbsTotal + tempTotLbs;
       this.customers[i] = randomCustomers;
       this.cups[i] = tempCups.toFixed(1);
       this.cupLbs[i] = tempCupLbs.toFixed(1);
       this.rawLbs[i] = tempRawLbs.toFixed(1);
       this.totLbs[i] = tempTotLbs.toFixed(1);
     }
+    this.cupLbsTotal = tempCupLbsTotal.toFixed(1);
+    this.rawLbsTotal = tempRawLbsTotal.toFixed(1);
+    this.dayLbsTotal = tempDayLbsTotal.toFixed(1);
   };
   this.calculator();
 }
@@ -52,10 +61,13 @@ tableEl.appendChild(theadEl);
 var trTopEl = document.createElement('tr');
 theadEl.appendChild(trTopEl);
 
-var td1TopEl = document.createElement('td');
+var td1TopEl = document.createElement('th');
 trTopEl.appendChild(td1TopEl);
 
-var td2TopEl = document.createElement('td');
+var emptyEl1 = document.createElement('th');
+trTopEl.appendChild(emptyEl1);
+
+var td2TopEl = document.createElement('th');
 td2TopEl.textContent = 'Coffee Pounds Required per Location per Hour (lbs)';
 td2TopEl.colSpan = 15;
 trTopEl.appendChild(td2TopEl);
@@ -67,6 +79,9 @@ var th1El = document.createElement('th');
 th1El.textContent = 'Location';
 trEl.appendChild(th1El);
 
+var emptyEl2 = document.createElement('th');
+trEl.appendChild(emptyEl2);
+
 function initTable(hoursString) {
   for (var i = 0; i < hoursString.length; i++) {
     var iEl = document.createElement('th');
@@ -76,19 +91,69 @@ function initTable(hoursString) {
 }
 initTable(hoursString);
 
+var totColEl = document.createElement('th');
+totColEl.textContent = 'Total (lbs)';
+trEl.appendChild(totColEl);
+
 function tablePopulate(location) {
   var rowEl = document.createElement('tr');
   tableEl.appendChild(rowEl);
 
-  var nameEl = document.createElement('td');
+  var nameEl = document.createElement('th');
   nameEl.textContent = location.location;
+  nameEl.rowSpan = 3;
   rowEl.appendChild(nameEl);
 
+  var hourlyTotEl = document.createElement('th');
+  hourlyTotEl.textContent = 'Total lbs';
+  rowEl.appendChild(hourlyTotEl);
+
+  var cupsRowEl = document.createElement('tr');
+  tableEl.appendChild(cupsRowEl);
+
+  var hourlyCupTotEl = document.createElement('th');
+  hourlyCupTotEl.textContent = 'Cup lbs';
+  cupsRowEl.appendChild(hourlyCupTotEl);
+
+  var rawRowEl = document.createElement('tr');
+  tableEl.appendChild(rawRowEl);
+
+  var hourlyRawTotEl = document.createElement('th');
+  hourlyRawTotEl.textContent = 'To-Go lbs';
+  rawRowEl.appendChild(hourlyRawTotEl);
+
   for (var i = 0; i < hoursString.length; i++) {
+
     var tdEl = document.createElement('td');
     tdEl.textContent = location.totLbs[i];
     rowEl.appendChild(tdEl);
+
+    var cupsTdEl = document.createElement('td');
+    cupsTdEl.textContent = location.cupLbs[i];
+    cupsRowEl.appendChild(cupsTdEl);
+
+    var rawTdEl = document.createElement('td');
+    rawTdEl.textContent = location.rawLbs[i];
+    rawRowEl.appendChild(rawTdEl);
   }
+  var totLbsEl = document.createElement('th');
+  totLbsEl.textContent = location.dayLbsTotal;
+  rowEl.appendChild(totLbsEl);
+
+  var cupLbsEl = document.createElement('th');
+  cupLbsEl.textContent = location.cupLbsTotal;
+  cupsRowEl.appendChild(cupLbsEl);
+
+  var rawLbsEl = document.createElement('th');
+  rawLbsEl.textContent = location.rawLbsTotal;
+  rawRowEl.appendChild(rawLbsEl);
+
+  var rowBreakEl = document.createElement('tr');
+  tableEl.appendChild(rowBreakEl);
+
+  var dataBreakEl = document.createElement('td');
+  dataBreakEl.colSpan = 17;
+  rowBreakEl.appendChild(dataBreakEl);
 }
 
 tablePopulate(pikePlaceMarket);
@@ -103,9 +168,16 @@ tablePopulate(websiteSales);
 var formEl = document.createElement('form');
 document.body.appendChild(formEl);
 
+var fieldsetEl = document.createElement('fieldset');
+formEl.appendChild(fieldsetEl);
+
+var legendEl = document.createElement('legend');
+legendEl.textContent = 'Add a new location';
+fieldsetEl.appendChild(legendEl);
+
 var formParagraph1El = document.createElement('label');
 formParagraph1El.textContent = 'Location';
-formEl.appendChild(formParagraph1El);
+fieldsetEl.appendChild(formParagraph1El);
 
 var formInput1El = document.createElement('input');
 formInput1El.type = 'text';
@@ -115,13 +187,13 @@ formInput1El.maxLength = 30;
 formInput1El.id = 'formLocation';
 formParagraph1El.appendChild(formInput1El);
 
-var dummyParagraphEl = document.createElement('p');
-dummyParagraphEl.id = 'result';
-formEl.appendChild(dummyParagraphEl);
+var dummyParagraph1El = document.createElement('p');
+dummyParagraph1El.id = 'result1';
+fieldsetEl.appendChild(dummyParagraph1El);
 
 var formParagraph2El = document.createElement('label');
 formParagraph2El.textContent = 'Minimum customers';
-formEl.appendChild(formParagraph2El);
+fieldsetEl.appendChild(formParagraph2El);
 
 var formInput2El = document.createElement('input');
 formInput2El.type = 'text';
@@ -131,9 +203,13 @@ formInput2El.maxLength = 30;
 formInput2El.id = 'minimumCustomers';
 formParagraph2El.appendChild(formInput2El);
 
+var dummyParagraph2El = document.createElement('p');
+dummyParagraph2El.id = 'result2';
+fieldsetEl.appendChild(dummyParagraph2El);
+
 var formParagraph3El = document.createElement('label');
 formParagraph3El.textContent = 'Maximum customers';
-formEl.appendChild(formParagraph3El);
+fieldsetEl.appendChild(formParagraph3El);
 
 var formInput3El = document.createElement('input');
 formInput3El.type = 'text';
@@ -143,9 +219,13 @@ formInput3El.maxLength = 30;
 formInput3El.id = 'maximumCustomers';
 formParagraph3El.appendChild(formInput3El);
 
+var dummyParagraph3El = document.createElement('p');
+dummyParagraph3El.id = 'result3';
+fieldsetEl.appendChild(dummyParagraph3El);
+
 var formParagraph4El = document.createElement('label');
 formParagraph4El.textContent = 'Cups per customer';
-formEl.appendChild(formParagraph4El);
+fieldsetEl.appendChild(formParagraph4El);
 
 var formInput4El = document.createElement('input');
 formInput4El.type = 'text';
@@ -155,9 +235,13 @@ formInput4El.maxLength = 30;
 formInput4El.id = 'cupsPerCustomer';
 formParagraph4El.appendChild(formInput4El);
 
+var dummyParagraph4El = document.createElement('p');
+dummyParagraph4El.id = 'result4';
+fieldsetEl.appendChild(dummyParagraph4El);
+
 var formParagraph5El = document.createElement('label');
 formParagraph5El.textContent = 'Pounds per customer';
-formEl.appendChild(formParagraph5El);
+fieldsetEl.appendChild(formParagraph5El);
 
 var formInput5El = document.createElement('input');
 formInput5El.type = 'text';
@@ -167,19 +251,23 @@ formInput5El.maxLength = 30;
 formInput5El.id = 'lbsPerCustomer';
 formParagraph5El.appendChild(formInput5El);
 
+var dummyParagraph5El = document.createElement('p');
+dummyParagraph5El.id = 'result5';
+fieldsetEl.appendChild(dummyParagraph5El);
+
 var formSubmitEl = document.createElement('button');
 formSubmitEl.textContent = 'Submit';
 formSubmitEl.id = 'button';
 formSubmitEl.type = 'button';
-formEl.appendChild(formSubmitEl);
+fieldsetEl.appendChild(formSubmitEl);
 
 function getLoc() {
-  var locField = document.formParagraph1El.value;
-  var result = document.dummyParagraphEl;
+  var locField = formParagraph1El.value;
+  // var result = document.dummyParagraphEl;
 }
 
 // var subButton = document.formSubmitEl;
-formSubmitEl.addEventListener('click', getLoc, false);
+formSubmitEl.addEventListener('click', getLoc(), false);
 // var formSubmitEl = document.createElement('input');
 // formSubmitEl.type = 'button';
 // formSubmitEl.name = 'location data';
